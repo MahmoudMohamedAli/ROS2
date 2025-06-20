9fa34c493731ded2eafa76bbfbc6a1a2d6199903
Mahmoud19931010Elkot <mahmoudalihack.0000@gmail.com>
2025-06-20 13:50:28 +0200

    add source file

---
+++ src.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(int argc, char * argv[])
+{
+  std::cout << "Hello, ROS2!" << std::endl;
+  std::cout << "This is a simple C++ program." << std::endl;
+  std::cout << "It does not require any ROS2 dependencies." << std::endl;
+  return 0;
+}
