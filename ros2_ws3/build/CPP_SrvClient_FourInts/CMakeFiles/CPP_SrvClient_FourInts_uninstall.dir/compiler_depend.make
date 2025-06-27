097e8c2e6d2b90a1f4bf464a1014bf2b8373ae94
Mahmoud19931010Elkot <mahmoudalihack.0000@gmail.com>
2025-06-27 12:32:06 +0200

    add action server and client

--- ros2_ws3/build/CPP_SrvClient_FourInts/CMakeFiles/CPP_SrvClient_FourInts_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for CPP_SrvClient_FourInts_uninstall.
-# This may be replaced when dependencies are built.
--- ros2_ws3/build/tutorial_interfaces/rosidl_generator_py/tutorial_interfaces/srv/__init__.py
+++
@@ -1,2 +0,0 @@
-from tutorial_interfaces.srv._add_four_ints import AddFourInts  # noqa: F401
-from tutorial_interfaces.srv._add_three_ints import AddThreeInts  # noqa: F401
