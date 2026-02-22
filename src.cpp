a17d64a2b42cb2d7207380091b9ba1644203d58b
MahmoudMohamedAli <mahmoudalielkout@gmail.com>
2026-02-23 00:16:34 +0100

    add line to src file

--- src.cpp
+++ src.cpp
@@ -4,5 +4,6 @@ int main(int argc, char * argv[])
   std::cout << "Hello, ROS2!" << std::endl;
   std::cout << "This is a simple C++ program." << std::endl;
   std::cout << "It does not require any ROS2 dependencies." << std::endl;
+  std::cout << "End of Test..!"<<std::endl;
   return 0;
 }
